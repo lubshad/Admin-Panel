8cfa328a134c22e405b9b481594eb7ea577ef24e
lubshad <lubshad4u4@gmail.com>
2021-12-21 16:01:28 +0530

    sidebar started

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './app.css';
+import Sidebar from './components/sidebar/Sidebar';
 import Topbar from './components/topbar/Topbar';
 
 function App() {
   return (
     <div className="App">
       <Topbar></Topbar>
+      <Sidebar></Sidebar>
     </div>
   );
 }
