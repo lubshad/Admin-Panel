e5032f96f8ccbea0520ebcb91ba912caa5d0481e
lubshad <lubshad4u4@gmail.com>
2021-12-21 18:47:50 +0530

    sidebar ui completed

---
+++ src/components/default_spacer/DefaultSpacer.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import "./default_spacer.scss"
+
+export  function DefaultSpacerVertical() {
+    return (
+        <div className='default-spacer-vertical'>
+            EmptySpace
+        </div>
+    )
+}
+
+
+
+export  function DefaultSpacerHorizontal() {
+    return (
+        <div className='default-spacer-horizontal'>
+            EmptySpace
+        </div>
+    )
+}
--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,5 +1,6 @@
-import { Analytics, GraphicEq, Home } from '@mui/icons-material'
+import { Analytics, Feedback, GraphicEq, Home, Mail, Message, Money, Person, Report, Store } from '@mui/icons-material'
 import React from 'react'
+import { DefaultSpacerVertical } from '../default_spacer/DefaultSpacer'
 import MenuItem from './components/MenuItem'
 import "./sidebar.scss"
 
@@ -9,10 +10,30 @@ export default function Sidebar() {
             <div className="sidebar-container">
                 <div className="menu-section">
                     <caption className="menu-section-title">Dashboard</caption>
-                    <MenuItem Icon = {Home} name ={"Home"}></MenuItem>
-                    <MenuItem Icon = {Analytics} name ={"Analytics"}></MenuItem>
-                    <MenuItem Icon = {GraphicEq} name ={"Sales"}></MenuItem>
+                    <MenuItem Icon={Home} name={"Home"}></MenuItem>
+                    <MenuItem Icon={Analytics} name={"Analytics"}></MenuItem>
+                    <MenuItem Icon={GraphicEq} name={"Sales"}></MenuItem>
                 </div>
+                <div className="menu-section">
+                    <caption className="menu-section-title">Quick Menu</caption>
+                    <MenuItem Icon={Person} name={"Users"}></MenuItem>
+                    <MenuItem Icon={Store} name={"Products"}></MenuItem>
+                    <MenuItem Icon={Money} name={"Transactions"}></MenuItem>
+                    <MenuItem Icon={Analytics} name={"Reports"}></MenuItem>
+                </div>
+                <div className="menu-section">
+                    <caption className="menu-section-title">Notifications</caption>
+                    <MenuItem Icon={Mail} name={"Mail"}></MenuItem>
+                    <MenuItem Icon={Feedback} name={"Feedback"}></MenuItem>
+                    <MenuItem Icon={Message} name={"Messages"}></MenuItem>
+                </div>
+                <div className="menu-section">
+                    <caption className="menu-section-title">Staff</caption>
+                    <MenuItem Icon={Mail} name={"Manage"}></MenuItem>
+                    <MenuItem Icon={Analytics} name={"Analytics"}></MenuItem>
+                    <MenuItem Icon={Report} name={"Report"}></MenuItem>
+                </div>
+                <DefaultSpacerVertical></DefaultSpacerVertical>
             </div>
         </div>
     )
